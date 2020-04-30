ca7ac909ba05f8b56a70ff23f650221ce112bb06
dgonzalezg <dmgonzalezg@uc.cl>
2020-04-30 18:06:26 -0400

    :bug: patch bug
    fixed: patch bug

--- burgers_api/urls.py
+++ burgers_api/urls.py
@@ -9,10 +9,10 @@ router = routers.DefaultRouter()
 #router.register(r'ingrediente', views.ingredients_list)
 urlpatterns = [
     path('', include(router.urls)),
-    path('hamburguesa/', views.burgers_list),
+    path('hamburguesa', views.burgers_list),
     path('hamburguesa/<pk>', views.burger_detail),
     path('hamburguesa/<int:burguer_id>/ingrediente/<int:ingredient_id>', views.burguer_update),
-    path('ingrediente/', views.ingredients_list),
+    path('ingrediente', views.ingredients_list),
     path('ingrediente/<pk>', views.ingredient_detail, name='ingredient_detail'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
