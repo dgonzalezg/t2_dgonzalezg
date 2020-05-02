2a6ebace6fd97fb665918759fa978088ad0a7be5
dgonzalezg <dmgonzalezg@uc.cl>
2020-05-02 12:20:34 -0400

    :boom: change migration
    feat: change migration

--- burgers_api/models.py
+++ burgers_api/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Ingredientes(models.Model):
-  nombre = models.CharField(max_length=60)
-  descripcion = models.CharField(max_length=60)
+  nombre = models.CharField(max_length=100)
+  descripcion = models.CharField(max_length=200)
 
   def __str__(self):
     return self.nombre
 
 
 class Hamburguesa(models.Model):
-  nombre = models.CharField(max_length=60)
+  nombre = models.CharField(max_length=100)
   precio = models.IntegerField()
-  descripcion = models.CharField(max_length=60)
-  imagen = models.CharField(max_length=100)
+  descripcion = models.CharField(max_length=200)
+  imagen = models.CharField(max_length=200)
   ingredientes = models.ManyToManyField(Ingredientes, blank=True)
   
   def __str__(self):
